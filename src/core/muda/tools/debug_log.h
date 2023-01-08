8a61547a0ef3c57be7f9498eaca02d13cab9afc2
MuGdxy <lxy819469559@gmail.com>
2023-01-08 13:22:20 +0800

    fix debug_log print => ::muda::print

--- src/core/muda/tools/debug_log.h
+++ src/core/muda/tools/debug_log.h
@@ -6,20 +6,20 @@
 #include "../muda_config.h"
 
 #define muda_kernel_printf(fmt, ...)                                           \
-    print("(%d|%d,%d|%d,%d|%d)-(%d|%d,%d|%d,%d|%d):" fmt,                      \
-          muda::block_idx().x,                                                 \
-          muda::grid_dim().x,                                                  \
-          muda::block_idx().y,                                                 \
-          muda::grid_dim().y,                                                  \
-          muda::block_idx().z,                                                 \
-          muda::grid_dim().z,                                                  \
-          muda::thread_idx().x,                                                \
-          muda::block_dim().x,                                                 \
-          muda::thread_idx().y,                                                \
-          muda::block_dim().y,                                                 \
-          muda::thread_idx().z,                                                \
-          muda::block_dim().z,                                                 \
-          __VA_ARGS__)
+    ::muda::print("(%d|%d,%d|%d,%d|%d)-(%d|%d,%d|%d,%d|%d):" fmt,              \
+                  muda::block_idx().x,                                         \
+                  muda::grid_dim().x,                                          \
+                  muda::block_idx().y,                                         \
+                  muda::grid_dim().y,                                          \
+                  muda::block_idx().z,                                         \
+                  muda::grid_dim().z,                                          \
+                  muda::thread_idx().x,                                        \
+                  muda::block_dim().x,                                         \
+                  muda::thread_idx().y,                                        \
+                  muda::block_dim().y,                                         \
+                  muda::thread_idx().z,                                        \
+                  muda::block_dim().z,                                         \
+                  __VA_ARGS__)
 
 #define muda_debug_trap()                                                      \
     if constexpr(::muda::trapOnError)                                          \
@@ -45,21 +45,22 @@
         muda_debug_trap();                                                     \
     }
 
-#define muda_kernel_debug_info(debugOption, fmt, ...)                                       \
-    if constexpr((debugOption))                                                             \
-    {                                                                                       \
-        print("(%d|%d,%d|%d,%d|%d)-(%d|%d,%d|%d,%d|%d):" fmt "(" #debugOption " = true)\n", \
-              muda::block_idx().x,                                                          \
-              muda::grid_dim().x,                                                           \
-              muda::block_idx().y,                                                          \
-              muda::grid_dim().y,                                                           \
-              muda::block_idx().z,                                                          \
-              muda::grid_dim().z,                                                           \
-              muda::thread_idx().x,                                                         \
-              muda::block_dim().x,                                                          \
-              muda::thread_idx().y,                                                         \
-              muda::block_dim().y,                                                          \
-              muda::thread_idx().z,                                                         \
-              muda::block_dim().z,                                                          \
-              __VA_ARGS__);                                                                 \
+#define muda_kernel_debug_info(debugOption, fmt, ...)                          \
+    if constexpr((debugOption))                                                \
+    {                                                                          \
+        ::muda::print("(%d|%d,%d|%d,%d|%d)-(%d|%d,%d|%d,%d|%d):" fmt           \
+                      "(" #debugOption " = true)\n",                           \
+                      ::muda::block_idx().x,                                   \
+                      ::muda::grid_dim().x,                                    \
+                      ::muda::block_idx().y,                                   \
+                      ::muda::grid_dim().y,                                    \
+                      ::muda::block_idx().z,                                   \
+                      ::muda::grid_dim().z,                                    \
+                      ::muda::thread_idx().x,                                  \
+                      ::muda::block_dim().x,                                   \
+                      ::muda::thread_idx().y,                                  \
+                      ::muda::block_dim().y,                                   \
+                      ::muda::thread_idx().z,                                  \
+                      ::muda::block_dim().z,                                   \
+                      __VA_ARGS__);                                            \
     }
