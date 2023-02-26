3fdded09b9bdb20d6b2a5d45af6e3603f0dea2ab
sailing-innocent <sailing-innocent@foxmail.com>
2023-02-26 22:29:27 +0800

    feat: add an option gui-enabled to enable gui

--- options.lua
+++ options.lua
@@ -59,7 +59,12 @@ option("playground")
     option_dev_core_only_related()
 option_end()
 
-
+option("gui-enabled")
+    set_default(false)
+    set_showmenu(true)
+    set_category("root menu/dev")
+    set_description("build for gui gallary. If you're the developer, you could enable this option for more intuitive examples.")
+option_end()
 -- *******************
 --
 -- module options
