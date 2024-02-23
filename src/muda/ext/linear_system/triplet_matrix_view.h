cd21946a8e5f6c4aef5740266880e6eb7efff129
MuGdxy <lxy819469559@gmail.com>
2024-02-23 12:33:05 +0800

    try improve triplet matrix view, add subrange

--- src/muda/ext/linear_system/triplet_matrix_view.h
+++ src/muda/ext/linear_system/triplet_matrix_view.h
@@ -24,8 +24,12 @@ class TripletMatrixViewBase : public ViewBase<IsConst>
 
   protected:
     // matrix info
-    int m_block_rows = 0;
-    int m_block_cols = 0;
+    int m_total_block_rows = 0;
+    int m_total_block_cols = 0;
+
+    // writable range: TODO
+    int2 m_sub_begin = {0, 0};
+    int2 m_sub_end   = {0, 0};
 
     // triplet info
     int m_triplet_index_offset = 0;
@@ -46,14 +50,16 @@ class TripletMatrixViewBase : public ViewBase<IsConst>
                                        auto_const_t<int>* block_row_indices,
                                        auto_const_t<int>* block_col_indices,
                                        auto_const_t<BlockMatrix>* block_values)
-        : m_block_rows(rows)
-        , m_block_cols(cols)
+        : m_total_block_rows(rows)
+        , m_total_block_cols(cols)
         , m_triplet_index_offset(triplet_index_offset)
         , m_triplet_count(triplet_count)
         , m_total_triplet_count(total_triplet_count)
         , m_block_row_indices(block_row_indices)
         , m_block_col_indices(block_col_indices)
         , m_block_values(block_values)
+        , m_sub_begin(0, 0)
+        , m_sub_end(rows, cols)
     {
         MUDA_ASSERT(triplet_index_offset + triplet_count <= total_triplet_count,
                     "TripletMatrixView: out of range, m_total_triplet_count=%d, "
@@ -66,8 +72,8 @@ class TripletMatrixViewBase : public ViewBase<IsConst>
     // explicit conversion to non-const
     MUDA_GENERIC ConstView as_const() const
     {
-        return ConstView{m_block_rows,
-                         m_block_cols,
+        return ConstView{m_total_block_rows,
+                         m_total_block_cols,
                          m_triplet_index_offset,
                          m_triplet_count,
                          m_total_triplet_count,
@@ -81,8 +87,8 @@ class TripletMatrixViewBase : public ViewBase<IsConst>
 
     MUDA_GENERIC auto subview(int offset, int count) const
     {
-        return ThisView{m_block_rows,
-                        m_block_cols,
+        return ThisView{m_total_block_rows,
+                        m_total_block_cols,
                         m_triplet_index_offset + offset,
                         count,
                         m_total_triplet_count,
@@ -102,8 +108,8 @@ class TripletMatrixViewBase : public ViewBase<IsConst>
 
     MUDA_GENERIC auto cviewer() const
     {
-        return CViewer{m_block_rows,
-                       m_block_cols,
+        return CViewer{m_total_block_rows,
+                       m_total_block_cols,
                        m_triplet_index_offset,
                        m_triplet_count,
                        m_total_triplet_count,
@@ -114,8 +120,8 @@ class TripletMatrixViewBase : public ViewBase<IsConst>
 
     MUDA_GENERIC auto viewer()
     {
-        return ThisViewer{m_block_rows,
-                          m_block_cols,
+        return ThisViewer{m_total_block_rows,
+                          m_total_block_cols,
                           m_triplet_index_offset,
                           m_triplet_count,
                           m_total_triplet_count,
@@ -129,14 +135,44 @@ class TripletMatrixViewBase : public ViewBase<IsConst>
     auto_const_t<int>* block_row_indices() { return m_block_row_indices; }
     auto_const_t<int>* block_col_indices() { return m_block_col_indices; }
 
+    auto sub_block(int2 offset, int2 size)
+    {
+        MUDA_ASSERT(m_sub_begin.x + offset.x + size.x <= m_sub_end,
+                    "TripletMatrixView: sub block is out of range, sub_begin.x=%d, offset.x=%d, size.x=%d, sub_end.x=%d",
+                    m_sub_begin.x,
+                    offset.x,
+                    size.x,
+                    m_sub_end.x);
+
+        MUDA_ASSERT(m_sub_begin.y + offset.y + size.y <= m_sub_end,
+                    "TripletMatrixView: sub block is out of range, sub_begin.y=%d, offset.y=%d, size.y=%d, sub_end.y=%d",
+                    m_sub_begin.y,
+                    offset.y,
+                    size.y,
+                    m_sub_end.y);
+
+        auto copy = *this;
+        copy.m_sub_begin.x += offset.x;
+        copy.m_sub_begin.y += offset.y;
+        copy.m_sub_end.x = copy.m_sub_begin.x + size.x;
+        copy.m_sub_end.y = copy.m_sub_begin.y + size.y;
+
+        return copy;
+    }
+
+    auto sub_block(int2 offset, int2 size) const
+    {
+        return as_const().sub_block(offset, size);
+    }
 
     // const access
     auto block_values() const { return m_block_values; }
     auto block_row_indices() const { return m_block_row_indices; }
     auto block_col_indices() const { return m_block_col_indices; }
 
-    auto block_rows() const { return m_block_rows; }
-    auto block_cols() const { return m_block_cols; }
+    auto total_block_rows() const { return m_total_block_rows; }
+    auto total_block_cols() const { return m_total_block_cols; }
+
     auto triplet_count() const { return m_triplet_count; }
     auto tripet_index_offset() const { return m_triplet_index_offset; }
     auto total_triplet_count() const { return m_total_triplet_count; }
@@ -160,6 +196,9 @@ class TripletMatrixViewBase<IsConst, Ty, 1> : public ViewBase<IsConst>
     // matrix info
     int m_rows = 0;
     int m_cols = 0;
+
+    int2 m_sub_begin = {0, 0};
+    int2 m_sub_end   = {0, 0};
 
     // triplet info
     int m_triplet_index_offset = 0;
@@ -191,6 +230,8 @@ class TripletMatrixViewBase<IsConst, Ty, 1> : public ViewBase<IsConst>
         , m_row_indices(row_indices)
         , m_col_indices(col_indices)
         , m_values(values)
+        , m_sub_begin(0, 0)
+        , m_sub_end(rows, cols)
     {
         MUDA_ASSERT(triplet_index_offset + triplet_count <= total_triplet_count,
                     "TripletMatrixView: out of range, m_total_triplet_count=%d, "
